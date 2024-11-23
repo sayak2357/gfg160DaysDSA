d04b3e51914b496853fd2a552fe282f36b202fdf
sayak2357 <sayak.chowdhury@alumni.iitgn.ac.in>
2024-11-23 20:24:20 +0530

    optimise stackBySellMaxOne code

--- day8StockBuyAndSellMaxOne/src/main/java/com/github/Solution.java
+++ day8StockBuyAndSellMaxOne/src/main/java/com/github/Solution.java
@@ -3,15 +3,12 @@ package com.github;
 public class Solution {
     public int maximumProfit(int prices[]) {
         int n = prices.length;
-        int minPrice = Integer.MAX_VALUE, maxPrice = Integer.MIN_VALUE, maxProfit = 0;
-        int[] minPricesArr =  new int[n];
-        for(int i=0;i<n;i++){
-            minPrice = Math.min(minPrice,prices[i]);
-            minPricesArr[i] = minPrice;
-        }
-        for(int i=n-1;i>=0;i--){
-            maxPrice = Math.max(maxPrice,prices[i]);
-            maxProfit = Math.max(maxProfit,maxPrice-minPricesArr[i]);
+        int minPrice = Integer.MAX_VALUE, maxProfit = 0;
+        for(int num:prices){
+            if(num<=minPrice)
+                minPrice = num;
+            else
+                maxProfit = Math.max(maxProfit,num-minPrice);
         }
         return maxProfit;
     }
