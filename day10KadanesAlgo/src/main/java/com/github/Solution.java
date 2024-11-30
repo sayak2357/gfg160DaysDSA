7bd2bf397a8be14656d402cc5a8177e28212d890
sayak2357 <sayak.chowdhury@alumni.iitgn.ac.in>
2024-11-30 19:44:37 +0530

    use better variable name

--- day10KadanesAlgo/src/main/java/com/github/Solution.java
+++ day10KadanesAlgo/src/main/java/com/github/Solution.java
@@ -3,10 +3,10 @@ package com.github;
 public class Solution {
     public int maxSubarraySum(int[] arr) {
         int n = arr.length;
-        int maxRunningsum = arr[0], maxSubsum = arr[0];
+        int maxEnding = arr[0], maxSubsum = maxEnding;
         for(int i=1;i<n;i++){
-            maxRunningsum = Math.max(maxRunningsum+arr[i],arr[i]);
-            maxSubsum = Math.max(maxSubsum,maxRunningsum);
+            maxEnding = Math.max(maxEnding+arr[i],arr[i]);
+            maxSubsum = Math.max(maxSubsum,maxEnding);
         }
         return maxSubsum;
 
